c5ed6c13d33bb31a85f3d1a3b692da1801d77288
Oscar Gutierrez <oscarfgutierrezo@gmail.com>
2022-03-25 14:42:24 -0500

    Add: content-01, content-02 and content-03 section

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        "background": "#F2F2F2",
+        "primary": "#2F1893",
+        "dark-violet": "#1E0E62",
+        "dark-violet-02": "#151439",
+        "light-violet": "#482BE7",
+        "pink": "#E93A7D",
+        "green": "#25DAC5"
+      },
+      fontFamily: {
+        "sans": "DM Sans"
+      },
+    },
+  },
+  plugins: [],
+}
